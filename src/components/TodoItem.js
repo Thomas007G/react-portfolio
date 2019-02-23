4371a601a098e2b84b1cde0027cad14c63c9382c
Thomas007G <thomas007galligan@gmail.com>
2019-02-23 19:17:47 +0000

    Added Components and imported

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+        <button>Home</button>
+      </div>
+    )
+  }
+}
+
+export default TodoItem
---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import NavBar from './NavBar.js'
+export class TodoList extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  }
+}
+
+export default TodoList
